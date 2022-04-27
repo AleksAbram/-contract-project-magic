abfec73c5358ffcd6c27a9ec2fba00ec1c7ae5a4
Nikita <nikita.gav96@gmail.com>
2022-04-27 18:23:22 +0300

    added db

---
+++ db/migrations/20220427151447-create-card.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      img: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      degree: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      user_city: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cards');
+  }
+};
