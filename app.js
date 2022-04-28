4b95e9cfb7fba59d52482a1c977e33eadb1af0fa
Nikita <nikita.gav96@gmail.com>
2022-04-28 20:55:15 +0300

    added shop

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const config = require('./config/config');
 const mainRoute = require('./route/main.route');
 const regRoute = require('./route/reg.route');
 const loginRoute = require('./route/login.route');
-const logoutRouter = require('./route/logout.router')
+const logoutRouter = require('./route/logout.router');
+const shopRoute = require('./route/shop.router')
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use('/', mainRoute);
 app.use('/', regRoute);
 app.use('/', loginRoute);
 app.use('/', logoutRouter);
+app.use('/', shopRoute);
 
 app.listen(PORT, () => console.log(`*** Server started at ${PORT} port ***`));
